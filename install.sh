270794c0d26611ebd6dc4d457ba1a73111db852d
Yesudeep Mangalapilly <yesudeep@google.com>
2012-04-27 10:08:55 +0530

    removes bashrc.

--- install.sh
+++ install.sh
@@ -20,6 +20,5 @@ PWD=`pwd`
 
 ln -s ${PWD}/profile ${HOME}/.profile
 ln -s ${PWD}/vimrc ${HOME}/.vimrc
-ln -s ${PWD}/bashrc ${HOME}/.bashrc
 ln -s ${PWD}/hgrc ${HOME}/.hgrc
 ln -s ${PWD}/.bashrc.d/ ${HOME}/.bashrc.d
