18d48eb6534e342234d2c072915832b8420d9b7c
Yesudeep Mangalapilly <yesudeep@google.com>
2012-12-10 01:35:42 +0530

    Fish prompt update. Extra echo causes breaking on linux.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -82,7 +82,7 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_prompt_host (set_color $fish_color_host)
   end
 
-  echo -s ''
+  #echo -s ''
   echo -s "$__fish_prompt_datetime $__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal"
   echo -s "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
   echo -s $git_info "$prompt_status" "$delim" ' '
