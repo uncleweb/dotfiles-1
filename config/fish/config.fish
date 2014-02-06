08758a544cf10db10c65e10493ab3c68b96f4073
Yesudeep Mangalapilly <yesudeep@google.com>
2014-02-06 15:54:54 +0530

    Clean up fish configuration.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,8 @@ if status --is-interactive
   set GOROOT /usr/local/go
   set GOBIN /usr/local/go/bin
   set NODE_BIN /usr/local/share/npm/bin
-  set PATH $GOBIN $NODE_BIN /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
+  set MYSQL_BIN /usr/local/mysql/bin
+  set EDITOR /usr/local/bin/vim
+  set PATH $GOBIN $MYSQL_BIN $NODE_BIN /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
 end
 
