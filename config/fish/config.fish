7ab4732674fbdcf17d049ad6550252b8ccfebf3d
Yesudeep Mangalapilly <yesudeep@google.com>
2014-07-02 08:32:17 +0530

    Hmm.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,12 @@
-if status --is-interactive 
+if status --is-interactive
   set PYTHONPATH $PYTHONPATH /usr/local/lib/python2.7/site-packages
   set GOROOT /usr/local/go
   set GOBIN /usr/local/go/bin
   set NODE_BIN /usr/local/share/npm/bin
   set MYSQL_BIN /usr/local/mysql/bin
   set EDITOR /usr/local/bin/vim
-  set PATH $GOBIN $MYSQL_BIN $NODE_BIN /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
-end
 
+  set LOCAL_VAR_BIN $HOME/var/bin
+
+  set PATH $LOCAL_VAR_BIN $GOBIN $MYSQL_BIN $NODE_BIN /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
+end
