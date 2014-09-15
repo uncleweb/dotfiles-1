1ff4944c4b6be6a484e176c90ecd81933be728d2
Yesudeep Mangalapilly <yesudeep@google.com>
2014-09-15 10:59:27 +0530

    Updates vim installer script.

--- install.sh
+++ install.sh
@@ -19,24 +19,23 @@
 PWD=`pwd`
 
 git submodule update --init --recursive
-#ln -s ${PWD}/profile ${HOME}/.profile
-#ln -s ${PWD}/emacs.d ${HOME}/.emacs.d
-ln -s ${PWD}/vimrc ${HOME}/.vimrc
-ln -s ${PWD}/vim ${HOME}/.vim
-ln -s ${PWD}/hgrc ${HOME}/.hgrc
-vim +BundleInstall +qall
-cd ~/.vim/bundle/Command-T/ruby/command-t && ruby extconf.rb && make && cd
-mkdir -p ~/.vim/autoload
-mkdir -p ~/.vim/ftplugin
 
+# Vim configuration.
+VIM_DIR=${HOME}/.vim
+
+
+ln -s ${PWD}/vimrc ${HOME}/.vimrc
+# ln -s ${PWD}/vim ${HOME}/.vim
+ln -s ${PWD}/hgrc ${HOME}/.hgrc
+
+mkdir -p ${VIM_DIR}/autoload ${VIM_DIR}/ftplugin
+curl -fLo ${VIM_DIR}/autoload/plug.vim \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+cd ~/.vim/autoload && curl -O https://raw.github.com/nsf/gocode/master/vim/autoload/gocomplete.vim
+cd ~/.vim/ftplugin && curl -O https://raw.github.com/nsf/gocode/master/vim/ftplugin/go.vim
 
 # Fish shell configuration.
 mkdir -p ~/.config/fish
 ln -s ~/.dotfiles/config/fish/functions ~/.config/fish/functions
 ln -s ~/.dotfiles/config/fish/config.fish ~/.config/fish/config.fish
-
-
-cd ~/.vim/autoload && curl -O https://raw.github.com/nsf/gocode/master/vim/autoload/gocomplete.vim
-cd ~/.vim/ftplugin && curl -O https://raw.github.com/nsf/gocode/master/vim/ftplugin/go.vim
-
-#ln -s ${PWD}/.bashrc.d/ ${HOME}/.bashrc.d
