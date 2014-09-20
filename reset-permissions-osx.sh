f2d4542e4befe7bd72c9d0351c81c9ed7913afe6
Yesudeep Mangalapilly <yesudeep@google.com>
2014-09-20 11:53:46 +0530

    Adds a script to reset permissions on the /usr/local/ subdirectories.

---
+++ reset-permissions-osx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo chown yesudeep \
+  /usr/local \
+  /usr/local/.git \
+  /usr/local/bin \
+  /usr/local/include \
+  /usr/local/etc \
+  /usr/local/share \
+  /usr/local/sbin \
+  /usr/local/include/osxfuse/fuse \
+  /usr/local/include/osxfuse
