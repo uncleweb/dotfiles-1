f75a441c79b20e475d2c0ed42be49efa0bf14db3
Yesudeep Mangalapilly <yesudeep@google.com>
2014-10-30 12:27:57 +0530

    Removes emacs configuration from dotfiles.

--- emacs.d/site-lisp/cloud-theme/cloud-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cloud-theme" "20120728" "Cloud color theme for GNU Emacs 24 [source: github]" (quote nil))
--- emacs.d/site-lisp/google-code-theme/google-code-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "google-code-theme" "20120728" "Google Code color theme for GNU Emacs 24 [source: github]" (quote nil))
