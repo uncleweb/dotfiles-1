9fc2013e9d760703d10b9d427ad2dde1a5194aff
Yesudeep Mangalapilly <yesudeep@gmail.com>
2014-11-30 04:53:33 +0530

    Go path updates.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,7 @@
 if status --is-interactive
   set PYTHONPATH $PYTHONPATH /usr/local/lib/python2.7/site-packages
-  set GOROOT /usr/local/go
-  set GOBIN /usr/local/go/bin
+  set GOROOT $HOME/var/go
+  set GOBIN $HOME/var/go/bin
   set NODE_BIN /usr/local/share/npm/bin
   set MYSQL_BIN /usr/local/mysql/bin
   set EDITOR /usr/local/bin/vim
