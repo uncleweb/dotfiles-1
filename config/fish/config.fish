f33a236eadcb92ac72f1eeba92db40f3f9ea0af8
Yesudeep Mangalapilly <yesudeep@gmail.com>
2014-11-29 22:29:43 +0530

    Adds google cloud sdk bin path to path environment variable for everybody.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,8 +5,11 @@ if status --is-interactive
   set NODE_BIN /usr/local/share/npm/bin
   set MYSQL_BIN /usr/local/mysql/bin
   set EDITOR /usr/local/bin/vim
-
+  set GOOGLE_CLOUD_SDK $HOME/google-cloud-sdk
+ 
+  # Mac OS X - Homebrew installs here.
+  set PATH /usr/local/opt/go/libexec/bin $PATH
   set LOCAL_VAR_BIN $HOME/var/bin
 
-  set PATH $LOCAL_VAR_BIN $GOBIN $MYSQL_BIN $NODE_BIN /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
+  set PATH $LOCAL_VAR_BIN $GOBIN $GOOGLE_CLOUD_SDK/bin $MYSQL_BIN $NODE_BIN /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
 end
