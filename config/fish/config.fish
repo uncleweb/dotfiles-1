fc1e404e3e115fa56e330cf0d268d4dabdc9e812
Yesudeep Mangalapilly <yesudeep@gmail.com>
2014-12-07 20:54:58 +0530

    Cleaner path.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,14 @@
 if status --is-interactive
   set EDITOR /usr/local/bin/vim
 
-  set PYTHONPATH $PYTHONPATH /usr/local/lib/python2.7/site-packages
-  set NODE_BIN /usr/local/share/npm/bin
-  set MYSQL_BIN /usr/local/mysql/bin
+  #set PYTHONPATH $PYTHONPATH /usr/local/lib/python2.7/site-packages
+  #set NODE_BIN /usr/local/share/npm/bin
+  #set MYSQL_BIN /usr/local/mysql/bin
 
+  # Google Cloud SDK installation directory.
   set GOOGLE_CLOUD_SDK $HOME/google-cloud-sdk
 
+  # Google Go installation directory.
   # The location where go is built is not the same as where it is installed.
   # Since $GOROOT is compiled into the binary, we need a way to override it in
   # the shell. According to the documentation, setting $GOROOT_FINAL does this
@@ -14,10 +16,18 @@ if status --is-interactive
   set -x GOROOT /usr/local/bhojo/go
   set -x GOPATH /usr/local/bhojo/golib
 
+  # Disabled.
   # Mac OS X - Homebrew installs here.
   # set PATH /usr/local/opt/go/libexec/bin $PATH
-  set LOCAL_VAR_BIN $HOME/var/bin
 
+  # Google Dart installation directory.
+  set DART_SDK $HOME/dart-sdk
+
+  set LOCAL_VAR_BIN $HOME/var/bin
+  
+  # Now set the correct PATH environment variable.
   set PATH /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /sbin /bin $PATH
-  set PATH $LOCAL_VAR_BIN $GOPATH/bin $GOROOT/bin $GOOGLE_CLOUD_SDK/bin $MYSQL_BIN $NODE_BIN $PATH
+  # set PATH $MYSQL_BIN $NODE_BIN $PATH
+  set PATH $DART_SDK/bin $GOPATH/bin $GOROOT/bin $GOOGLE_CLOUD_SDK/bin $PATH
+  set PATH $LOCAL_VAR_BIN $PATH
 end
