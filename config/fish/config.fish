3510c462b0aa2800d999d019710976c7015f0334
Yesudeep Mangalapilly <yesudeep@google.com>
2015-01-31 12:06:07 +0530

    testing keychain configuration with fish.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,13 @@
 if status --is-interactive
-  set -x LC_ALL en_US.UTF-8 
-  set -x LANG en_US.UTF-8 
-  
+  set -x LC_ALL en_US.UTF-8
+  set -x LANG en_US.UTF-8
+
   set EDITOR /usr/local/bin/vim
 
   # Google Android SDK Path.
   # Buck requires this to be exported to avoid hardcoding in the repository.
   set -x ANDROID_SDK $HOME/Library/Android/sdk
-  
+
   # Google Cloud SDK installation directory.
   set -x GOOGLE_CLOUD_SDK $HOME/google-cloud-sdk
 
@@ -36,8 +36,52 @@ if status --is-interactive
     $PATH
 
 
+  # for f in $HOME/.ssh/*.pub
+  #     eval (keychain --eval (basename -s .pub $f))
+  # end
+  # keychain --nogui --clear ~/.ssh/id_rsa
+
   function gtop
     set top_dir (git rev-parse --show-toplevel)
     printf "%s" $top_dir
   end
+end
+
+
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV
+    . $SSH_ENV > /dev/null
+    ssh-add
+end
+
+function test_identities
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
+if [ -n "$SSH_AGENT_PID" ]
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV > /dev/null
+    end
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else
+        start_agent
+    end
 end
